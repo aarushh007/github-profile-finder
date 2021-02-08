c860b1258a02e5f18d28c29963cc639c5ae27650
aarushh007 <aarushh007@gmail.com>
2021-02-08 12:10:03 -0500

    master

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState, useEffect} from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+    const [name, setName] = useState('loading');
+    const [img, setImg] = useState('blank');
+    useEffect(()=>{
+        fetch(`https://api.github.com/users/aarushh007`).then((res)=>res.json()).then((data)=>{setUser(data)})
+    },[])
+    const setUser = (data) => {
+        setName(data.login);
+        setImg(data.avatar_url);
+    }
+    const search = () => {
+        let val = document.getElementById('name').value;
+        fetch(`https://api.github.com/users/${val}`).then((res)=>res.json()).then((data)=>{setUser(data)})
+        document.getElementById('name').value = '';
+    }
+    const page = `https://github.com/${name}`;
+    return (
+        <div>
+            <h1>Search for GitHub users</h1>
+            < br/>
+            <h3>{name}</h3>
+            <img src={img} alt='' width='100px'/>
+            <br />
+            <a href={page} target="_blank">GitHub Page</a>
+            <br />
+            <br />
+            <input id='name' placeholder='search for users'/>
+            <button onClick={search}>Search</button>
+        </div>
+    )
 }
 
 export default App;
